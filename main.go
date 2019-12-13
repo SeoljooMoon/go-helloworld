5087a5cf4a2495005bd8d403e40994d6b4806a11
Seoljoo Moon <seoljoomoon@gmail.com>
2019-12-13 20:19:26 +0900

    use custom library

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/moonwoou/go-stringutil"
 )
 
 func main() {
 	fmt.Println("Hello World")
+
+	val := stringutil.Reverse("ABCDEF")
+	fmt.Println(val)
 }
